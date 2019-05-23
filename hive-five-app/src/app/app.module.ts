b6e3fd14e9349b85c766d0028ea6c2227cc2d8d0
Phatho Pukwana <>
2019-05-23 17:30:00 +0200

    Added, materials, bootstraps, a navbar component

--- hive-five-app/src/app/app.module.ts
+++ hive-five-app/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+// Angular wrapper for socket.io client
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+// Angular Material Imports
+import {MatToolbarModule, MatIconModule} from '@angular/material';
+
+// Declarations
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// Address where scoket server is listening
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SocketIoModule.forRoot(config),
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
