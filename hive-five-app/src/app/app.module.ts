d49b1cdbe94aee78198fd3f2ab269e91c0a35454
Phatho Pukwana <>
2019-05-24 16:26:09 +0200

    bootstrap app shell

--- hive-five-app/src/app/app.module.ts
+++ hive-five-app/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+// Declarations
 import { AppComponent } from './app.component';
+import { TrackListComponent } from './track-list/track-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// Server listening on port 4444
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackListComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
