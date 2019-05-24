b4cdccfe1a1e69bb551753f34b39b3ab4c094812
Phatho Pukwana <>
2019-05-24 11:15:42 +0200

    regenerated app to use scss

--- hive-five-app/src/app/app.module.ts
+++ hive-five-app/src/app/app.module.ts
@@ -1,34 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-// Angular wrapper for socket.io client
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-
-// Angular Material Imports
-import {MatToolbarModule, MatIconModule, MatSidenavModule} from '@angular/material';
-
-// Declarations
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-
-// Address where scoket server is listening
-const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidenavComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    SocketIoModule.forRoot(config),
-    MatToolbarModule,
-    MatIconModule,
-    MatSidenavModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
